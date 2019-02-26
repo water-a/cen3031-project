1f7f24e4b8e008f01b84f8ba5f8b23d2c28dfe6a
mfileman <madisonfileman@gmail.com>
2019-02-26 12:28:07 -0500

    made a button, just to check that I know how to use git

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { PromiseProvider } from 'mongoose';
 
-class App extends Component {
+class Slider extends Component {
+
+  renderContentSlider(cnt){
+    return(
+      <button className = 'content-slider'> 
+        {cnt}
+      </button>
+
+    )
+   
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.renderContentSlider("madison")}
       </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
     );
   }
 }
 
-export default App;
+export default Slider;
