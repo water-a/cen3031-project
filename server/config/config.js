34768a7cdf11c181023572a939808186f5beea2e
Andy Liu <a.liu@ufl.edu>
2019-03-19 17:36:12 -0400

    Added new files and removed old files

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     db: {
-        uri: ''
+        uri: 'mongodb+srv://root:E8SrbGdypUpdq9QD@cluster0-dxfpv.mongodb.net/database?retryWrites=true'
     },
     port: 3001
 }
--- server/config/init.js
+++ server/config/init.js
@@ -3,15 +3,19 @@ const express = require('express'),
       morgan = require('morgan'),
       config = require('./config');
 
+mongoose.connect(config.db.uri, {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.once('open', console.info.bind(console, 'DATABASE: Successfully connected to database!'));
+db.on('error', console.error.bind(console, 'DATABASE [error]: '))
+    
 var app = express();
 
-// Logs requests sent to server
+// Logs requests
 app.use(morgan('dev'));
 
 // Example route
-app.use('/api', require('../routes/example.server.routes.js'));
+app.use('/api', require('../routes/api.routes'));
 
 // Open up and listen to port listed in config file
-app.listen(config.port, () => {
-    console.log('Launched backend on http://localhost:3001');
-});
+app.listen(config.port, console.info.bind(console, 'SERVER: Launched backend on http://localhost:3001'));
