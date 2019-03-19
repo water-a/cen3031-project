01a0eb7719018cfd00bae0c44c9f4eaac249829b
mfileman <madisonfileman@gmail.com>
2019-03-19 18:49:10 -0400

    added stuff playing around

--- client/src/components/List/ImageUpload.js
+++ client/src/components/List/ImageUpload.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './ImageUpload.css';
 
 
 class ImageUpload extends React.Component
@@ -11,17 +12,8 @@ class ImageUpload extends React.Component
         this.state = {file: '', image_url: ''}
     }
 
-
-    _handleSubmit(e) 
-    //will be able to mess with the middleware to send the actual file to the backend! :)
+    _handleImageChange(e)
     {
-        e.preventDefault();
-        // TODO: do something with the file!!! -> this.state.file
-        console.log('handle uploading-', this.state.file);
-      }
-
-
-    _handleImageChange(e){
         let reader = new FileReader(); // allows for us to read a file
         let file = e.target.files[0];  // ??? holds the actual file 
 
@@ -33,7 +25,23 @@ class ImageUpload extends React.Component
         }
 
         reader.readAsDataURL(file)
-    }
+    } 
+
+
+    _handleSubmit(e) 
+    //will be able to mess with the middleware to send the actual file to the backend! :)
+    {
+        e.preventDefault();
+        // TODO: do something with the file!!! -> this.state.file
+        //ALSO make it so that it goes to the next page, upload box function has it 
+
+        this.setState({
+          file: '',
+          imagePreviewUrl: ''
+        });
+        console.log('handle uploading-', this.state.file);
+        this.props.next();
+      }
 
 
     render() {
@@ -41,25 +49,38 @@ class ImageUpload extends React.Component
         let $imagePreview = null;
         if (imagePreviewUrl) {
           $imagePreview = (<img src={imagePreviewUrl} alt = "this was uploaded"/>);
-        } else {
-          $imagePreview = (<div className="previewText">Please select an Image for Preview</div>);
         }
     
-        return (
-          <div className="previewComponent">
-            <form onSubmit={(e)=>this._handleSubmit(e)}>
-              <input name="photo_uploaded" className="fileInput" 
-              //you will then be able to access the photo via the express request object using the name photo_uploaded
-              // req.photo_uploaded
-              //onChange even happens when the value or state of the thing has changed
-              //so onChange, call the handle image change function, passes the ...?
-                type="file" 
-                onChange={(e)=>this._handleImageChange(e)} /> 
+       
 
-              <button className="submitButton" 
+        //you will then be able to access the photo via the express request object using the name photo_uploaded
+        // req.photo_uploaded
+        //onChange even happens when the value or state of the thing has changed
+        //so onChange, call the handle image change function, passes the ...?
+
+        return (
+          <div className="upload-wrapper">
+
+<label for="file-upload" class="photo-choice-btn">
+    Upload photo
+</label>
+
+
+              <input
+                //className="photo-choice-btn"
+                id ="file-upload"
+                type="file" 
+                //name="photo_uploaded"
+                //onChange={(e)=>this._handleImageChange(e)} 
+              /> 
+
+              <button className="upload-button" 
                 type="submit" 
-                onClick={(e)=>this._handleSubmit(e)}>Upload Image</button>
-            </form>
+                onClick={(e)=>this._handleSubmit(e)}>Submit
+              </button>
+
+
+
             <div className="imgPreview">
               {$imagePreview}
             </div>
@@ -68,6 +89,6 @@ class ImageUpload extends React.Component
     }
     
     //ReactDOM.render(<ImageUpload/>, document.getElementById("mainApp"));
-
-}
+}   
+export default ImageUpload;
 
