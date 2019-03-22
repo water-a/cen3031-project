57bcfe449e4c837f7430538838709edc55adcff2
alexfortes <alejandro.fortes@ufl.edu>
2019-03-22 17:59:57 -0400

    Minimal Stuff in the Help Page

--- client/src/pages/Help/Help.js
+++ client/src/pages/Help/Help.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import { TextField } from 'office-ui-fabric-react/lib/TextField';
+import './Help.css';
+
+
 class Help extends Component {
   render() {
     return (
       <div>
-        help is on the way! hello this is help
+        {/*
+        <div className="left">
+        left
+        </div>
+        <div className="right">
+        right
+        </div>
+        */}
+        <ul class = "container">
+          <div class = "left">
+            <a href="#something">FAQ</a>
+            <a href="#something">Whatever</a>
+          </div>
+          <div class = "right">
+            Content
+          </div>
+        </ul>
       </div>
     );
   }
