0b7ac165b7dbdb9eb9546b951f50aea05db2d8c8
mfileman <madisonfileman@gmail.com>
2019-03-22 17:58:55 -0400

    updates to Selection components, base Choice component within the Selection file

--- client/src/pages/OrderForm/OrderForm.js
+++ client/src/pages/OrderForm/OrderForm.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import ImageUpload from '../../components/ImageUpload';
+import Selection from '../../components/Selection';
 import './OrderForm.css';
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <ImageUpload /> */}
+        <Selection />
       </div>
     );
   }
