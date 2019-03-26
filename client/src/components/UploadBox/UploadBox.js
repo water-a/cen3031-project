b717053951b01c8e332b4f607b7bbef9a676ac2e
Andy Liu <a.liu@ufl.edu>
2019-03-26 09:52:23 -0400

    Moved UploadBox into its own component + Working on Carousel component

---
+++ client/src/components/UploadBox/UploadBox.js
@@ -0,0 +1,21 @@
+import 'UploadBox.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+class App extends Component {
+    render() {
+      return (
+        <div className="UploadBox">
+          <h2>Start ordering your print now</h2>
+            <br />
+            <div className="Dropzone">
+                <div className="Message">
+                    <FontAwesomeIcon icon="plus" size="6x" />
+                    Drop files here or click to upload
+                </div>
+            </div>
+        </div>
+      );
+    }
+  }
+  
--- client/src/pages/OrderForm/OrderForm.js
+++ client/src/pages/OrderForm/OrderForm.js
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
 import './OrderForm.css';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import UploadBox from '../../components/UploadBox';
 
 class App extends Component {
   render() {
     return (
       <div className="Order">
-        {/* Move into seperate component! */}
-        {/* Make it look prettier? */}
-        <h2>Start ordering your print now</h2>
-        <br />
-        <div className="Dropzone">
-          <div className="Message">
-            <FontAwesomeIcon icon="plus" size="6x" />
-            Drop files here or click to upload
-          </div>
-        </div>
+        <UploadBox />
       </div>
     );
   }
