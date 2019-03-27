3dee7d770841c903c13c854f70664a980fd34055
Andy Liu <a.liu@ufl.edu>
2019-03-27 11:38:53 -0400

    Update Carousel.js

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -4,10 +4,17 @@ import './Carousel.css';
 class Carousel extends Component {
     constructor(props){
         super(props);
-        
+        this.state = {
+            index: 0,
+            slides: props.slides
+        };
     }
     render() {
-        
+        return (
+            <div className="Carousel">
+                
+            </div>
+        );
     }
 }
 
