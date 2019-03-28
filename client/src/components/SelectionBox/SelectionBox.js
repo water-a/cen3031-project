f196b979e82f1a95da9dcda19ea7c96486a33ec5
Andy Liu <a.liu@ufl.edu>
2019-03-28 12:55:58 -0400

    Pushed SelectionBox update

--- client/src/components/SelectionBox/SelectionBox.js
+++ client/src/components/SelectionBox/SelectionBox.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
+import classNames from 'classnames/bind';
 import './SelectionBox.css';
 
 class SelectionBox extends Component {
     constructor(props){
         super(props);
         this.state = {
+            index: null,
             items: this.props.items
         };
-        this._renderItem = this.props.render
+        this._renderItem = this.props.render;
+        this._onSelect = this.props.onSelect;
     }
-    render() {
+    _select = () => {
+        
+    }
+    render(){
         return (
             <div className="SelectionBox">
-                {this.state.items.map((item, index) => this._renderItem(item, index))}
+                {this.state.items.map((item, index) => (
+                    <div key={index} onClick={this._select.bind()} className={classNames(['Selection', index === this.state.index && 'Active'])}>
+                        {this._renderItem(item, index)}
+                    </div>
+                ))}
             </div>
         );
     }
--- client/src/pages/OrderForm/OrderForm.js
+++ client/src/pages/OrderForm/OrderForm.js
@@ -6,7 +6,44 @@ import { UploadBox } from '../../components/UploadBox';
 import { Carousel } from '../../components/Carousel';
 import { OrderFormContext } from '../../components/OrderFormContext';
 
+// Going to fetch this from the website
+const Materials = ['Gloss', 'Matte', 'Custom'];
+const Sizes = [
+  {
+    name: 'Large',
+    size: {
+      height: 100,
+      width: 100
+    } 
+  },
+  {
+    name: 'Medium',
+    size: {
+      height: 50,
+      width: 50
+    } 
+  },
+  {
+    name: 'Small',
+    size: {
+      height: 10,
+      width: 10
+    } 
+  },
+  {
+    name: 'Custom'
+  }
+];
+
 class OrderForm extends Component {
+  _renderMaterialSelection = (item, index) => <b>{item}</b>
+  _renderSizeSelection = (item, index) => (
+    <>
+      <b>{item.name}</b>
+      {item.size && <><br />{item.size.height + 'x' + item.size.width}</>}
+    </>
+  )
+
   render() {
     return (
       <OrderFormContext.Provider className="Order">
@@ -20,8 +57,8 @@ class OrderForm extends Component {
             {
               slide: <SelectionBox
                 key="Material" 
-                items={['Gloss', 'Matte', 'Custom']} 
-                render={(item, index) => <div key={index} className="Selection"><b>{item}</b></div>} 
+                items={Materials} 
+                render={this._renderMaterialSelection} 
               />,
               name: 'Material',
               icon: 'sticky-note',
@@ -30,39 +67,17 @@ class OrderForm extends Component {
               slide: 
                 <SelectionBox 
                   key="Size"
-                  items={[
-                    {
-                      name: 'Large',
-                      size: {
-                        height: 100,
-                        width: 100
-                      } 
-                    },
-                    {
-                      name: 'Medium',
-                      size: {
-                        height: 50,
-                        width: 50
-                      } 
-                    },
-                    {
-                      name: 'Small',
-                      size: {
-                        height: 10,
-                        width: 10
-                      } 
-                    },
-                    {
-                      name: 'Custom'
-                    }
-                  ]} 
-                  render={(item, index) => <div key={index} className="Selection"><b>{item.name}</b>{item.size && <><br />{item.size.height + 'x' + item.size.width}</>}</div>} 
+                  items={Sizes} 
+                  render={this._renderSizeSelection} 
                 />,
               name: 'Size',
               icon: 'ruler-combined',
             },
             {
-              slide: <div><h1 style={{textAlign: "center"}}>Your estimate is</h1><span style={{fontSize: "5rem", color: "#00F4AC", textAlign: "center", display: "block"}}>$100</span></div>,
+              slide: <div>
+                      <h1 style={{textAlign: "center"}}>Your estimate is</h1>
+                      <span style={{fontSize: "5rem", color: "#00F4AC", textAlign: "center", display: "block"}}>$100</span>
+                    </div>,
               name: 'Estimate',
               icon: 'dollar-sign',
             }
