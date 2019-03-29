8dc894ce25527f677ffc769cabe305badc17a12a
Andy Liu <a.liu@ufl.edu>
2019-03-29 05:27:51 -0400

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,5 @@ module.exports = {
     db: {
         uri: 'mongodb+srv://root:E8SrbGdypUpdq9QD@cluster0-dxfpv.mongodb.net/database?retryWrites=true'
     },
-    port: 3001
+    port: process.env.PORT || 3001
 }
