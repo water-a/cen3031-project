dbe7631ce8fa4f7d032091302427e53dbc5a6726
Andy Liu <a.liu@ufl.edu>
2019-03-29 01:48:48 -0400

    Update UploadBox.js

--- client/src/components/UploadBox/UploadBox.js
+++ client/src/components/UploadBox/UploadBox.js
@@ -6,15 +6,22 @@ class UploadBox extends Component {
     _onClick = () => {
         this.refs.upload.click();
     }
+    _onChange = (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        let file = event.target.files[0];
+        
+    }
     render() {
         return (
             <div className="UploadBox">
                 <div className="Dropzone" onClick={this._onClick}>
                     <div className="Message">
-                        <input type="file" ref="upload"/>
+                        <input type="file" ref="upload" onChange={this._onChange} />
                         <FontAwesomeIcon icon="plus" size="6x" />
                         Drop files here or click to upload
                     </div>
+
                 </div>
             </div>
         );
