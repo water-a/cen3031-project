db5910b4c240cb00ec3e087d029816ff8533d559
Andy Liu <a.liu@ufl.edu>
2019-03-29 05:16:01 -0400

    Build for deployment

---
+++ client/build/precache-manifest.b750ce4660aadc15194701671a048943.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "13782423991702c8dd1e",
+    "url": "/static/css/main.bcd70c57.chunk.css"
+  },
+  {
+    "revision": "13782423991702c8dd1e",
+    "url": "/static/js/main.89df5045.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2218d3f7b9e887334c5b",
+    "url": "/static/js/2.37f5760c.chunk.js"
+  },
+  {
+    "revision": "5886378f093f18b60e2a1e31cea3944a",
+    "url": "/static/media/logo.5886378f.png"
+  },
+  {
+    "revision": "128be613709fd411861f6b6d2eb90e4a",
+    "url": "/static/media/profile.128be613.jpg"
+  },
+  {
+    "revision": "aa5308a663a1c7e03362255186fc749f",
+    "url": "/index.html"
+  }
+];
