5625cab14a9993dbd8bb0f9cd9dd9c09eebfd952
Andy Liu <a.liu@ufl.edu>
2019-03-29 01:43:23 -0400

    Updated Uploadbox

--- client/src/components/UploadBox/UploadBox.js
+++ client/src/components/UploadBox/UploadBox.js
@@ -3,11 +3,15 @@ import './UploadBox.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class UploadBox extends Component {
+    _onClick = () => {
+        this.refs.upload.click();
+    }
     render() {
         return (
             <div className="UploadBox">
-                <div className="Dropzone">
+                <div className="Dropzone" onClick={this._onClick}>
                     <div className="Message">
+                        <input type="file" ref="upload"/>
                         <FontAwesomeIcon icon="plus" size="6x" />
                         Drop files here or click to upload
                     </div>
