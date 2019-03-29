1b032001946254e66328330c8a42eb889826e21d
Andy Liu <a.liu@ufl.edu>
2019-03-29 15:41:26 -0400

    Added fake popup for site + Rebuilt

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -17,6 +17,7 @@ class Contact extends Component {
         
         <br />
         <Button
+          onClick={this._submit}
           className="button"
           buttonType={ ButtonType.primary} 
           allowDisabledFocus={true}
